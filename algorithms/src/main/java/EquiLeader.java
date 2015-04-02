24cdeed70186174de5542500aae8af4bdbe266a6
Yuriy Bondaruk <yuriy.bondaruk@skelia.com.ua>
2015-04-02 17:27:15 +0300

    Add FindDigits

--- algorithms/src/main/java/EquiLeader.java
+++ algorithms/src/main/java/EquiLeader.java
@@ -1,6 +1,22 @@
 import java.util.Date;
 import java.util.Stack;
 
+/**
+ * The equilibrium index of a sequence is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes. For example, in a sequence A:
+    A[0]=-7 A[1]=1 A[2]=5 A[3]=2 A[4]=-4 A[5]=3 A[6]=0
+ 3 is an equilibrium index, because:
+    A[0]+A[1]+A[2]=A[4]+A[5]+A[6]
+ 6 is also an equilibrium index, because:
+    A[0]+A[1]+A[2]+A[3]+A[4]+A[5]=0
+ (The sum of zero elements is zero) 7 is not an equilibrium index - because it is not a valid index of sequence A.
+ If you still have doubts, here is a precise definition:
+    The integer k is an equilibrium index of a sequence A[0],A[1]..,A[n-1] if and only if 0<= k and sum(A[0..(k-1)])=sum(A[(k+1)..(n-1)]).
+    Assume the sum of zero elements is equal to zero.
+
+ Write a function
+    int equi(int A[], int n)
+ that, given a sequence, returns its equilibrium index (any) or -1 if no equilibrium index exists. Assume that the sequence may be very long.
+ * */
 public class EquiLeader {
 
     public static void main(String[] args) {
---
+++ algorithms/src/main/java/FindDigits.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Problem Statement
+
+ You are given a number N. Find the digits in this number that exactly divide N and display their count.
+ For N = 24, there are 2 digits - 2 & 4. Both these digits exactly divide 24. So our answer is 2.
+
+ Note
+
+ If the same number is repeated twice at different positions, it should be counted twice, e.g., For N=122, 2 divides 122 exactly and occurs at ones' and tens' position. So it should be counted twice. So for this case, our answer is 3.
+ Division by 0 is undefined.
+
+ Input Format
+
+ The first line contains T (number of test cases) followed by T lines (each containing an integer N).
+
+ Constraints
+ 1 <=T <= 15
+ 0 < N < 1010
+
+ Output Format
+
+ For each test case, display the count of digits in N that exactly divide N in separate line.
+ */
+public class FindDigits {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            int numberOfTests = Integer.parseInt(in.readLine());
+            for (int i = 0; i < numberOfTests; i++) {
+                String numberStr = in.readLine();
+                int number = Integer.parseInt(numberStr);
+                char[] chars = numberStr.toCharArray();
+                int result = 0;
+                for (int j = 0; j < chars.length; j++) {
+                    int digit = Character.getNumericValue(chars[j]);
+                    if (digit != 0 && number % digit == 0) {
+                        result++;
+                    }
+                }
+                System.out.println(result);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
